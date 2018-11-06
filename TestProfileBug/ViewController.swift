f580dae0b6ab369fcf8e7583a8cf9c48db8f06c9
Neil García Vargas <Neil@Neils-MacBook-Pro.local>
2018-11-05 21:58:27 -0600

    initial commit

---
+++ TestProfileBug/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProfileBug
+//
+//  Created by Neil García Vargas on 11/5/18.
+//  Copyright © 2018 Neil García Vargas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
